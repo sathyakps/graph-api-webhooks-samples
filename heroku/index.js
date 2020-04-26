6edd5c039b7e2b857ff8b0b8875377e2983d0460
sathyakps <sathyakps@yahoo.com>
2020-04-26 10:50:26 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -37,16 +37,13 @@ app.get(['/facebook', '/instagram'], function(req, res) {
 });
 
 app.post('/facebook', function(req, res) {
-  console.log('Facebook request body:', req.body);
 
   if (!req.isXHubValid()) {
-    console.log('Warning - request header X-Hub-Signature not present or invalid');
     res.sendStatus(401);
     return;
   }
 
-  console.log('request header X-Hub-Signature validated');
-  console.log(req.body)
+  console.log(JSON.stringyfy(req.body))
   received_updates.push(req.body);
   res.sendStatus(200);
 });
